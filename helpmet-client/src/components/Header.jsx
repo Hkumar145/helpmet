18f7ec41f6a9d3e96b7fa3e4540ef149edf6d66c
Dejigray <promiseolajide@gmail.com>
2024-10-11 13:01:48 -0700

    header

--- helpmet-client/src/components/Header.jsx
+++ helpmet-client/src/components/Header.jsx
@@ -4,27 +4,33 @@ import { useSelector } from 'react-redux'
 const Header = () => {
   const {currentUser} = useSelector((state) => state.user);
   return (
-    <div className='bg-emerald-500'>
+    <div className='bg-white'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
-                <h1>Helpmet</h1>
+                <img src='./images/Group 7.svg'/>
             </Link>
             
             <ul className='flex gap-4'>
                 <Link to='/'>
-                  <li>Home</li>
+                  <li style={{color: 'black'}}>Dashboard</li>
                 </Link>
                 <Link to='/about'>
-                  <li>About</li>
+                  <li style={{color: 'black'}}>Incident Report</li>
                 </Link>
                 <Link to='/equipmentcheck'>
-                  <li>Equipmentcheck</li>
+                  <li style={{color: 'black'}}>Analytics</li>
+                </Link>
+                <Link>
+                  <li style={{color: 'black'}}>Alert</li> 
+                </Link>
+                <Link>
+                  <li style={{color: 'black'}}>Equipment Check</li>
                 </Link>
                 <Link to='/profile'>
                   {currentUser ? (
                     <img src={currentUser.profilePicture} alt="profile" className='h-7 w-7 rounded-full object-cover'/>
                   ):(
-                    <li>Login</li>
+                    <li style={{color: 'black'}}>Login</li>
                   )}
                 </Link>
             </ul>
