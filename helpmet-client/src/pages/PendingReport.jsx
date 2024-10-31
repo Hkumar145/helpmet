3154836e511d2c8d41cf8848086d559306a6aabd
ccyeung <ccyeung22@gmail.com>
2024-10-31 12:35:34 -0700

    date issue, responsiveness of tables
    fix minor date issue
    adjust responsiveness of tables

--- helpmet-client/src/pages/PendingReport.jsx
+++ helpmet-client/src/pages/PendingReport.jsx
@@ -43,7 +43,7 @@ const PendingReport = () => {
   return (
     <div className='flex flex-col gap-4'>
       <div className='flex flex-row items-center justify-between'>
-        <h1 className='text-white text-2xl'>Pending Report</h1>
+        <h1 className='text-lg text-white md:text-2xl'>Pending Report</h1>
         <button
             onClick={handleViewCompletedReports}
             className='bg-green-700 text-white p-3 mt-0 rounded-lg text-center hover:opacity-90 max-w-40'
@@ -53,18 +53,18 @@ const PendingReport = () => {
       </div>
 
       <div className="overflow-x-auto">
-        <table className="min-w-full bg-gray-800 text-white mt-4 rounded-lg">
+        <table className="min-w-full bg-gray-800 text-white mt-4 rounded-lg text-sm">
           <thead>
             <tr>
               {/* <th className="px-4 py-2">Report ID</th> */}
               {/* <th className="px-4 py-2">Severity</th> */}
-              <th className="px-4 py-2">Status</th>
-              <th className="px-4 py-2">Location</th>
-              <th className="px-4 py-2">Date of Injury</th>
+              <th className="px-2 py-2 md:px-4">Status</th>
+              <th className="px-0 py-2 md:px-4">Location</th>
+              <th className="px-0 py-2 md:px-4">Date of Injury</th>
               {/* <th className="px-4 py-2">Injured Employee</th> */}
               {/* <th className="px-4 py-2">Report Date</th> */}
-              <th className="px-4 py-2">Reported By</th>
-              <th className="px-4 py-2"></th>
+              <th className="px-0 py-2 md:px-4">Reported By</th>
+              <th className="px-2 py-2 md:px-4"></th>
             </tr>
           </thead>
           <tbody className='text-center'>
@@ -72,13 +72,13 @@ const PendingReport = () => {
               <tr key={report.reportID || report._id || `report-${index}`} className="border-t border-gray-700">
                 {/* <td className="px-4 py-2">{report.reportID ? report.reportID : "N/A"}</td> */}
                 {/* <td className="px-4 py-2">{severityMapping[report.severity]}</td> */}
-                <td className="px-4 py-2">{report.status}</td>
-                <td className="px-4 py-2">{report.locationID}</td>
-                <td className="px-4 py-2">{new Date(report.dateOfInjury).toLocaleDateString()}</td>
+                <td className="px-2 py-2 md:px-4">{report.status}</td>
+                <td className="px-0 py-2 md:px-4">{report.locationID}</td>
+                <td className="px-0 py-2 md:px-4">{new Date(report.dateOfInjury).toLocaleDateString()}</td>
                 {/* <td className="px-4 py-2">{report.injuredEmployeeFirstName}<br />({report.injuredEmployeeID})</td> */}
                 {/* <td className="px-4 py-2">{new Date(report.reportDate).toLocaleDateString()}</td> */}
-                <td className="px-4 py-2">{report.reportByFirstName}<br />({report.reportBy})</td>
-                <td className="px-4 py-2">
+                <td className="px-0 py-2 md:px-4">{report.reportByFirstName}<br />({report.reportBy})</td>
+                <td className="px-2 py-2 md:px-4">
                   <button
                     onClick={() => handleViewDetails(report._id)}
                     className='bg-purple-600 text-white p-2 rounded hover:bg-purple-800'
