a81d97aa64406fa43d4bbd5c70073d478745186e
ccyeung <ccyeung22@gmail.com>
2024-11-14 21:30:06 -0800

    confirm-delete btn

--- helpmet-client/src/pages/PendingReportDetails.jsx
+++ helpmet-client/src/pages/PendingReportDetails.jsx
@@ -132,8 +132,8 @@ const PendingReportDetails = () => {
         <>
           <ToastContainer position="top-right" />
           <div className="flex flex-col gap-4 max-w-6xl mx-auto w-full">
-            <div className="flex flex-col md:flex-row justify-between items-center gap-4 text-xs">
-              <div className="flex flex-row items-center justify-between w-[100%]">
+            <div className="flex flex-col md:flex-row justify-between items-center gap-4 text-xs px-2">
+              <div className="flex flex-row items-center justify-between w-full px-2">
                 <button
                   onClick={() => navigate(-1)}
                   className="flex items-center py-2 px-4 bg-white rounded text-xs text-black mt-0 gap-2 border text-nowrap mr-4 hover:bg-[#D9D6FE] hover:text-[#6938EF]"
@@ -180,7 +180,8 @@ const PendingReportDetails = () => {
                   ))}
                 </div>
               ) : (
-                <p>No image available</p>
+                // <p>No image available</p>
+                <div></div>
               )}
             </div>
 
