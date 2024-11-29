e77285d040fb8570631c97a7715551faa2aa15d7
ccyeung <ccyeung22@gmail.com>
2024-11-28 18:32:30 -0800

    landing page image

--- helpmet-client/src/pages/Home.jsx
+++ helpmet-client/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
           <p className='data'>Report, analyze, and act on injury data effortlessly with our user-friendly platform, designed to make workplace safety management easier, proactive, and more efficient.</p>
         </div>
         <div className='slimages'>
-        <img src="../../images/LandingpageImages/GroupEverything.svg" alt="" style={{height:'100vh'}}/>
+        <img src="/images/LandingpageImages/GroupEverything.svg" alt="" style={{height:'100vh'}}/>
         
         {/* <img src="../../images/Clocklan.svg" alt="" />
         <img src="../../images/Heatmaplan.svg" alt="" />
@@ -23,14 +23,14 @@ const Home = () => {
         <img src="../../images/Rectangle 1-1 lan.svg" alt="" /> */}
       </div>
       </div>
-      <div className="except"><img src="../../images/LandingpageImages/HelloWorld.svg" alt="" /></div>
+      <div className="except"><img src="/images/LandingpageImages/HelloWorld.svg" alt="" /></div>
       <div className='slogansec'>
       <div className="firstb inv">
         <h1 className='lightsl'>One platform</h1>
         <h1 className='boldsl'>Incident Report</h1>
         <p className='data'>Safety reporting has never been easier or more efficient. Our inbuilt incident report form allows you to submit and access critical information directly through your email, ensuring every detail is at your fingertips whenever and wherever you need it.</p>
       </div>
-      <div className="slimages landscape lanepor"><img src="../../images/LandingpageImages/incidentReportlan.png" alt="" /></div>
+      <div className="slimages landscape lanepor"><img src="/images/LandingpageImages/incidentReportlan.png" alt="" /></div>
       </div>
       
       <div className='sloganthir'>
@@ -38,7 +38,7 @@ const Home = () => {
          <h1 className='lightsl'>Track {/*<span className='boldsl'>Safety</span>*/}</h1> 
         <h1 className='boldsl'>Safety equipment check</h1>
       </div>
-        <div className="slimages lanepor"><img src="../../images/LandingpageImages/safetyequiplan.svg" alt="" /></div>
+        <div className="slimages lanepor"><img src="/images/LandingpageImages/safetyequiplan.svg" alt="" /></div>
       
       </div>
       
@@ -48,34 +48,34 @@ const Home = () => {
         <h1 className='boldsl'>Injury Analytics</h1>
         <p className='data'>Take a proactive approach to workplace safety with Helpmet’s advanced Injury Analytics Dashboard. This dashboard transforms data into actionable insights, giving you a clear view of trends, high-risk areas, and potential hazards across your organization.</p>
       </div>
-      <div className="slimages landscape lanepor"><img src="../../images/LandingpageImages/injuryanalan.svg" alt="" /></div>
+      <div className="slimages landscape lanepor"><img src="/images/LandingpageImages/injuryanalan.svg" alt="" /></div>
       </div>
       
       <div className="sloganfif">
       <h1 className='lightsl'>Analyze</h1>
       <div className="gr1">
         <div className="anal1">
-          <img src="../../images/LandingpageImages/Heatmaplan.svg" alt="" />
+          <img src="/images/LandingpageImages/Heatmaplan.svg" alt="" />
             <p className='data'>Injury Heat Maps provide a real-time geographical view of incident hotspots, helping you allocate resources effectively.</p>
             </div>
           <div className="anal2">
-            <img src="../../images/LandingpageImages/siteinjlan.svg" alt="" className='slofifimg' />
+            <img src="/images/LandingpageImages/siteinjlan.svg" alt="" className='slofifimg' />
             <p className="data">Status and Severity Indicators to prioritize response based on injury severity and location, ensuring timely action for high-risk incidents.</p>
           </div>
       </div>
 
       <div className="gr2">
         <div className="anal3">
-          <img src="../../images/LandingpageImages/Overviewlan.svg" alt="" className='slofifimg' />
+          <img src="/images/LandingpageImages/Overviewlan.svg" alt="" className='slofifimg' />
             <p className="data keep">Look into the statistics of your weekly data</p>
         </div>
         <div className="anal4">
-          <img src="../../images/LandingpageImages/Safetychelan.svg" alt="" className='slofifimg' />
+          <img src="/images/LandingpageImages/Safetychelan.svg" alt="" className='slofifimg' />
             <p className="data keep">Ensures compliance by monitoring scheduled checks to avoid missed inspections.</p>
         </div>
         
         <div className="anal5">
-          <img src="../../images/LandingpageImages/epiprolan.svg" alt="" className='slofifimg' />
+          <img src="/images/LandingpageImages/epiprolan.svg" alt="" className='slofifimg' />
             <p className="data keep">Anticipates risk patterns, enabling preventative measures before escalation.</p>
           </div>
         </div>
@@ -94,7 +94,7 @@ const Home = () => {
             <h1 className='boldsl'>Overview</h1>
             <p className='data'>This dashboard provides a quick view of weekly injury trends, category projections, and detailed incident logs with filtering through graph, helping you prioritize safety actions and reduce future risks effectively.</p>
           </div>
-          <div className="slimages landscape lanepor sev sevspesh"><img src="../../images/LandingpageImages/graphlan.svg" alt="" /></div>
+          <div className="slimages landscape lanepor sev sevspesh"><img src="/images/LandingpageImages/graphlan.svg" alt="" /></div>
         </div>
 
       <div className="sam2">
@@ -103,7 +103,7 @@ const Home = () => {
           <h1 className='boldsl'>Safety Equipment</h1>
           <p className='data'>This dashboard provides a clear overview of scheduled vs. completed checks, locations, and responsible personnel, ensuring compliance and maintaining a safe work environment effortlessly.</p>
         </div>
-        <div className="slimages landscape lanepor sevspesh seveig"><img src="../../images/LandingpageImages/safelan.svg" alt="" /></div>
+        <div className="slimages landscape lanepor sevspesh seveig"><img src="/images/LandingpageImages/safelan.svg" alt="" /></div>
         </div>
       </div>
       
@@ -117,7 +117,7 @@ const Home = () => {
           <h1 className='boldsl'>Alerts</h1>
           <p className='data'>Tailor alerts to reach the right people at the right time, enhancing safety and responsiveness across your team.</p>
         </div>
-        <div className="slimages landscape lanepor"><img src="../../images/LandingpageImages/notiflan.svg" alt="" /></div>
+        <div className="slimages landscape lanepor"><img src="/images/LandingpageImages/notiflan.svg" alt="" /></div>
         </div>
       </div>
       {/* <img src="../../images/Harshdeep kaur.svg" alt="" /> */}
